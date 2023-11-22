448d65de1de9fb0ee25eb77fb4f7fb00265100ff
igor <igor.ramos.c@hotmail.com>
2023-11-22 02:15:59 -0500

    navbar and menu list

--- src/pages/FotoPage.jsx
+++ src/pages/FotoPage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Navbar from "../component/Navbar";
 
 function FotoPage (){
     return (
-        <div>Foto Page dfdfdfdffdfd</div>
+        <div>
+            <Navbar/>
+            
+        </div>
     )
 }
 
